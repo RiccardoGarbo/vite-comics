2cbe6fe4dcbbfd9b79c833072ee79ed684676921
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-01 14:13:13 +0100

    general css style

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,27 @@ export default {
 
 <template></template>
 
-<style></style>
+<style>
+/*Generics*/
+
+* {
+    padding: 0 auto;
+    margin: 0 auto;
+    box-sizing: border-box;
+}
+
+body {
+    min-height: 100vh;
+}
+
+/*Utils*/
+.container {
+    width: 1080px;
+    margin: 0 auto;
+}
+
+img {
+    max-width: 100%;
+    display: block;
+}
+</style>
